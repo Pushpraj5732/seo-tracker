e0d108f0eba3f5cb80ff3ba61bc8e52d623cb0a7
Pushpraj5732 <153991103+Pushpraj5732@users.noreply.github.com>
2026-05-29 12:10:55 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,16 @@ import { rankTrackingDaily } from './cron/cron.js';
 const app=express();
 
 connectDB();    
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://seo-tracker-frontend-j6ilw51mb-raj5732s-projects.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true
+  })
+);
 app.use(express.json());
 app.use("/api/auth",authRoute)
 app.use("/api/rank",rankRouter)
@@ -19,7 +28,7 @@ app.get('/',(req,res)=>{
     res.send("hello world")
 })
 rankTrackingDaily();
-const Port=process.env.Port  ||5000;
+const Port=process.env.PORT   ||5000;
 app.listen(Port,()=>{
     console.log(`server listen ${Port}`)
 })
